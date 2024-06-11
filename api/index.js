e59e0ea89e8cd2b4deda5c423c4ad404b543677e
coder7475 <robiulhossain7475@gmail.com>
2024-06-11 20:24:59 +0600

    feat: refactor index.js

--- api/index.js
+++ api/index.js
@@ -6,34 +6,34 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-// MongoDB and mongoose connect
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 // middlewares
 app.use(cors());
 app.use("/public", express.static(`${process.cwd()}/public`));
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 // routes
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/public/index.html");
 });
+// MongoDB and mongoose connect
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
+main().catch((err) => console.log(err));
 
-app.post("/api/shorturl", function (req, res) {
-  console.log(req.body.url)
-  res.json({
-    original_url: "https://www.google.com",
-    short_url: 1
-  })
-})
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("connected to MongoDB...");
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+  // shortner api
+  app.post("/api/shorturl", function (req, res) {
+    res.json({
+      original_url: "https://www.google.com",
+      short_url: 1,
+    });
+  });
+
+  app.listen(port, function () {
+    console.log(`Listening on port ${port}....`);
+  });
+}
