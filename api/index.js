6e7cb70c111a4cabbd172e03faa6913faf234fd1
Robiul Hossain <robiulhossain7475@gmail.com>
2024-06-11 08:14:30 +0000

    feat: insatall mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
 const app = express();
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
